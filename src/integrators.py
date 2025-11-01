7a3014612b77eaca624619d229a743fc82185130
Max Carlin <maca3369@colorado.edu>
2025-11-01 14:55:25 -0600

    added leapfrog

--- src/integrators.py
+++ src/integrators.py
@@ -47,6 +47,10 @@ def om_step(state, _, params): #scan expects an x input along with carry/state e
 @partial(jax.jit,
           static_argnums=(2,))  # N_steps through record_H static
 def omelyan_core_scan(mom_x0, phi_x0, params: HMCParams):
+    """
+    One Omelyan trajectory of N_steps.
+    If record_H -> also return H history (shape (N_steps+1, batch))
+    """
     eps = params.eps
     N_steps = params.N_steps
     lam = params.lam
@@ -55,10 +59,6 @@ def omelyan_core_scan(mom_x0, phi_x0, params: HMCParams):
     shift = params.shift
     spatial_axes = params.spatial_axes
     record_H = params.record_H
-    """
-    One Omelyan trajectory of N_steps.
-    If record_H -> also return H history (shape (N_steps+1, batch))
-    """
     # pre-compute initial energy if Hamiltonian history is desired
     if record_H:
       S0, _, _ = action_core(phi_x0, lam, kappa, D, shift, spatial_axes)
@@ -77,3 +77,59 @@ def omelyan_core_scan(mom_x0, phi_x0, params: HMCParams):
       return mom_fx, phi_fx, H_hist
     return mom_fx, phi_fx
 
+
+def leap_step(state, _, params):
+	mom_x_p,phi_x_p = state
+
+	#I1 first half step; phi updates
+	phi_x_p = phi_x_p + params.eps/2 * mom_x_p
+	#I2 pi updates whole step
+	grad_s = grad_action_core(phi_x_p, params.lam, params.kappa, params.D, params.shift, params.spatial_axes)
+	# grad_s = Phi4Lattice._grad_action_core(phi_x_p, lam, kappa, D)
+	mom_x_p = mom_x_p - params.eps*grad_s
+	#I1 second half step; phi updates again
+	phi_x_p = phi_x_p + params.eps/2 * mom_x_p
+
+	# compute updated H after step
+	if params.record_H:
+		S_p, _,_ = action_core(phi_x_p, params.lam, params.kappa, params.D, params.shift, params.spatial_axes)
+		H_p = hamiltonian_kinetic_core(mom_x_p, params.spatial_axes) + S_p
+
+		return (mom_x_p, phi_x_p), H_p
+	return (mom_x_p, phi_x_p), None
+
+
+
+@staticmethod
+@partial(jax.jit, static_argnums=range(2, 10)) #static: N_steps...spatial
+# @partial(jax.jit, static_argnums=(3, 4, 6,7,8,9)) #static: N_steps...spatial
+# @partial(jax.jit, static_argnums=range(4, 10)) #static: lam...spatial
+def leapfrog_core_scan(mom_x0, phi_x0,  params: HMCParams):
+	eps = params.eps
+	N_steps = params.N_steps
+	lam = params.lam
+	kappa = params.kappa
+	D = params.D
+	shift = params.shift
+	spatial_axes = params.spatial_axes
+	record_H = params.record_H
+
+                        
+	# compute initial H if history is desired
+	if record_H:
+		S0, _, _ = action_core(phi_x0, lam, kappa, D, shift, spatial_axes)
+		H0 = hamiltonian_kinetic_core(mom_x0, spatial_axes) + S0
+
+	#run leap_step for N_steps     
+	(mom_fx, phi_fx), H_hist = lax.scan(
+          lambda s, _: leap_step(s, _, params),
+            (mom_x0, phi_x0),xs=None, length=params.N_steps)
+     
+	# Infer length from shape of xs so that I can vmap with dynamic scan outside of class
+	# xs = jnp.zeros(N_steps)
+	# (mom_fx, phi_fx), H_hist = lax.scan(leap_step, (mom_x0, phi_x0), xs = xs)
+	# concat initil H0
+	if record_H:
+		H_hist = jnp.concatenate((H0[None], H_hist), axis=0)
+		return mom_fx, phi_fx, H_hist
+	return mom_fx, phi_fx
