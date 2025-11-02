17b8cf7ffc3d6f2a646418b33e21eb860e99bc05
Sevio Stanton <Sevio.Stanton@colorado.edu>
2025-11-02 00:21:19 -0600

    cleaned style of action.py

--- src/action.py
+++ src/action.py
@@ -2,7 +2,8 @@ import jax
 import jax.numpy as jnp
 from functools import partial
 
-@partial(jax.jit, static_argnums=(3,4,5))
+
+@partial(jax.jit, static_argnums=(3, 4, 5))
 def action_core(phi_x: jnp.ndarray,
                 lam: float, kappa: float,
                 D: int,
@@ -28,41 +29,47 @@ def action_core(phi_x: jnp.ndarray,
     for mu in range(D):
         ax = mu + shift
         # +mu -> +1; -mu -> -1, no need for factor of 2 in action/kinetic term
-        K += (phi_x*(jnp.roll(phi_x,1,axis=ax)+jnp.roll(phi_x,-1,axis=ax))).sum(axis = spatial_axes)
+        K += (phi_x * (jnp.roll(phi_x, 1, axis=ax)
+              + jnp.roll(phi_x, -1, axis=ax))).sum(axis=spatial_axes)
 
-    K *= - kappa #total kinetic
-    U = (phi_x**2 + lam * (phi_x**2 -1.0)**2).sum(axis = spatial_axes) #total potential
+    K *= - kappa  # total kinetic
+    # total potential
+    U = (phi_x ** 2
+         + lam * (phi_x ** 2 - 1.0) ** 2).sum(axis=spatial_axes)
 
     W = -K/kappa
     S = K + U
     return S, K, W
 
-@partial(jax.jit, static_argnums=(1,2,3,4,5))
+
+@partial(jax.jit, static_argnums=(1, 2, 3, 4, 5))
 def grad_action_core(phi_x, lam, kappa, D, shift, spatial_axes):
     # total_action returns a scalar for jax.grad
     def total_action(phi):
         S_vals, _, _ = action_core(phi,
-                                                lam, kappa,
-                                                D, shift, spatial_axes)
+                                   lam, kappa,
+                                   D, shift, spatial_axes)
         return jnp.sum(S_vals)
-    #should compute grad(S) for both singular or batched configs w/out axis error
-    #that occured from other method
+    # should compute grad(S) for both singular
+    # or batched configs w/out axis error
+    # that occured from other method
     return jax.grad(total_action)(phi_x)
-
 
 
 def grad_action(self, phi_x, lam, kappa, D, shift, spatial_axes):
     if phi_x is None:
-        phi_x =phi_x
+        phi_x = phi_x
     return grad_action_core(phi_x, lam, kappa, D, shift, spatial_axes)
+
 
 def _magnetization_core(phi_x, D):
     '''
     Pure JIT’d kernel
     returns array of magnetizations for each field configuration in phi_x
     '''
-    m_array = phi_x.sum(axis = tuple(range(1,D+1)))
+    m_array = phi_x.sum(axis=tuple(range(1, D+1)))
     return m_array
+
 
 def magnetization(phi_x, D):
     '''
@@ -70,12 +77,12 @@ def magnetization(phi_x, D):
     '''
     return _magnetization_core(phi_x, D)
 
+
 def binder_cumulant(phi_x, D):
     """
     returns Binder cumulant, but only for batched fields
     """
     m = magnetization(phi_x, D)
-    m4_ave = (m**4).mean()
-    m2_ave = (m**2).mean()
-    return m4_ave/(m2_ave**2)
-
+    m4_ave = (m ** 4).mean()
+    m2_ave = (m ** 2).mean()
+    return m4_ave / (m2_ave ** 2)
