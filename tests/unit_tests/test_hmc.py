bb04172a38ab209c8cccfdbb7b63a3ed007538e6
Max Carlin <maca3369@colorado.edu>
2025-11-03 16:30:21 -0700

    linting

--- tests/unit_tests/test_hmc.py
+++ tests/unit_tests/test_hmc.py
@@ -1,11 +1,13 @@
 import sys
-sys.path.append('src')
+sys.path.append('src')  # noqa
 import unittest
 from src import hmc, params
 import jax.numpy as jnp
 import jax.random as random
 
-class TestHMC(unittest.TestCase): 
+
+class TestHMC(unittest.TestCase):
+
     def setUp(self):
         self.key = random.PRNGKey(0)
         self.H_old = jnp.array([1.0, 2.0, 3.0])
@@ -14,8 +16,8 @@ class TestHMC(unittest.TestCase):
         self.phi_prime = 2 * jnp.ones((3, 4))
         self.mom_old = jnp.zeros((3, 4))
         self.mom_prime = jnp.full((3, 4), 9.0)
-    
-    def test_HMC_core_accept_lower_H(self): 
+
+    def test_HMC_core_accept_lower_H(self):
         # Should always accept everything when H_prime < H_old
         result = hmc.HMC_core(self.H_old,
                               self.H_prime,
@@ -53,18 +55,17 @@ class TestHMC(unittest.TestCase):
             self.key
         )
 
-
         self.assertTrue(jnp.all(phi_acc == self.phi_old))
         self.assertTrue(jnp.all(mom_acc == self.mom_old))
         self.assertTrue(jnp.all(mask == 0))
 
     def test_MD_traj_bad_integrator_raises(self):
-        
+
         parameters = params.HMCParams(
             integrator="fakes_integrator",
             lam=1.0, kappa=1.0, D=2, shift=None,
             spatial_axes=(1,), lat_shape=(2, 2),
-            N_steps = 10, xi = 0.1931833, eps = 0.085,
+            N_steps=10, xi=0.1931833, eps=0.085,
             metropolis=True, record_H=False
         )
         state = (self.mom_old, self.phi_old)
@@ -74,5 +75,5 @@ class TestHMC(unittest.TestCase):
                           state, key_pair, parameters)
 
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     unittest.main()
